d1c87e73b52299970470a7c3415c7ad55135bca2
Lee Inho <inhoking@kookmin.ac.kr>
2023-11-18 23:42:19 +0900

    [backend] feat. initial commit

---
+++ src/main/java/com/travel/travelSpot/TravelSpotApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelSpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/travelSpot/TravelSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.travelSpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
